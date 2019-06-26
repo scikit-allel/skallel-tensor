6a359b217ed8b3065f36f6bd9f0efaf8ade45282
Alistair Miles <alimanfoo@googlemail.com>
2019-06-26 22:22:22 +0100

    add TODOs

--- src/skallel/model/oo.py
+++ src/skallel/model/oo.py
@@ -12,6 +12,8 @@ class GenotypeArray(object):
     def __init__(self, data):
         """TODO"""
 
+        # TODO support hdf5-like data
+
         # check type
         if isinstance(data, np.ndarray):
             self._fn = fn_numpy
@@ -67,10 +69,23 @@ class GenotypeArray(object):
 
     def is_hom(self):
         """TODO"""
+        # TODO support allele argument
         return self._fn.genotype_array_is_hom(self.data)
+
+    # TODO is_het
+    # TODO is_call
+    # TODO to_n_ref
+    # TODO to_n_alt
+    # TODO to_allele_counts
+    # TODO to_haplotypes
+    # TODO __repr__
+    # TODO display
+    # TODO map_alleles
+    # TODO max
 
     def count_alleles(self, max_allele):
         """TODO"""
+        # TODO support subpop arg
         # TODO wrap the result as AlleleCountsArray
         return self._fn.genotype_array_count_alleles(self.data, max_allele)
 
@@ -85,11 +100,74 @@ class GenotypeArray(object):
     # TODO select_samples_by_mask
     # TODO take
     # TODO compress
-    # TODO to_allele_counts
+    # TODO concatenate
+
+
+# TODO HaplotypeArray
+# TODO n_variants
+# TODO n_haplotypes
+# TODO __getitem__
+# TODO take
+# TODO compress
+# TODO concatenate
+# TODO is_called
+# TODO is_missing
+# TODO is_ref
+# TODO is_alt
+# TODO is_call
+# TODO to_genotypes
+# TODO count_alleles
+# TODO map_alleles
+# TODO prefix_argsort
+# TODO distinct
+# TODO distinct_counts
+# TODO distinct_frequencies
+# TODO display
+# TODO __repr__
+
+
+# TODO AlleleCountsArray
+# TODO __add__
+# TODO __sub__
+# TODO n_variants
+# TODO n_alleles
+# TODO __getitem__
+# TODO compress
+# TODO take
+# TODO concatenate
+# TODO to_frequencies
+# TODO allelism
+# TODO max_allele
+# TODO is_variant
+# TODO is_non_variant
+# TODO is_segregating
+# TODO is_non_segregating
+# TODO is_singleton
+# TODO is_doubleton
+# TODO is_biallelic
+# TODO is_biallelic_01
+# TODO map_alleles
+# TODO display
+# TODO __repr__
+
+
+# TODO GenotypeAlleleCountsArray
 
 
 # TODO Callset
+# TODO __getitem__
+
+
 # TODO ContigCallset
-# TODO HaplotypeArray
-# TODO AlleleCountsArray
-# TODO GenotypeAlleleCountsArray
+# TODO __getitem__
+# TODO select_variants_by_id
+# TODO select_variants_by_position
+# TODO select_variants_by_region
+# TODO select_variants_by_index
+# TODO select_variants_by_mask
+# TODO select_samples_by_id
+# TODO select_samples_by_index
+# TODO select_samples_by_mask
+# TODO take
+# TODO compress
+# TODO concatenate?
