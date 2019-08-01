964dd93aad7dfc019840e353c75d330cdccced32
Alistair Miles <alimanfoo@googlemail.com>
2019-08-01 15:43:03 +0100

    add __all__ (#27)

--- src/skallel_tensor/__init__.py
+++ src/skallel_tensor/__init__.py
@@ -29,3 +29,25 @@ from .api import (
 from . import numpy_backend
 from . import dask_backend
 from . import cuda_backend
+
+__all__ = [
+    'genotypes_locate_hom',
+    'genotypes_locate_het',
+    'genotypes_locate_call',
+    'genotypes_count_alleles',
+    'genotypes_to_called_allele_counts',
+    'genotypes_to_missing_allele_counts',
+    'genotypes_to_allele_counts',
+    'genotypes_to_allele_counts_melt',
+    'genotypes_to_major_allele_counts',
+    'genotypes_to_haplotypes',
+    'allele_counts_to_frequencies',
+    'allele_counts_allelism',
+    'allele_counts_max_allele',
+    'select_slice',
+    'select_indices',
+    'select_mask',
+    'select_range',
+    'select_values',
+    'concatenate',
+]
